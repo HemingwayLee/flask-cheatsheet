ae8db09c1b25083c4641f1d2e2f04d554e68a924
LeeHemingway <coherence0815@gmail.com>
2021-01-12 15:11:15 +0900

    add environment variable

--- hello/hello.py
+++ hello/hello.py
@@ -1,8 +1,13 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return('Hello, World!')
+  return 'Hello, World!'
 
+
+@app.route('/env')
+def hello_abc():
+  return f"Hello, {os.environ.get('FOOBAR', 'No env')}"
 
